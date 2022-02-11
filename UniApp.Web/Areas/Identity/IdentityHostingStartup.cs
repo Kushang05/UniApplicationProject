72518fa0f86452d2f060444249b9654ef968396c
KUSHANG Suratwala <suratwalakushang@gmail.com>
2022-02-11 17:47:15 +0530

    Modified Login features

---
+++ UniApp.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniApp.Web.Data;
+using UniApp.Web.Models;
+
+[assembly: HostingStartup(typeof(UniApp.Web.Areas.Identity.IdentityHostingStartup))]
+namespace UniApp.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
